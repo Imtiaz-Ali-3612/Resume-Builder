504b6a686a0f6d3cc5b290ea2b02ba2147ead39b
Imtiaz-Ali-3612 <60825273+Imtiaz-Ali-3612@users.noreply.github.com>
2020-06-21 19:21:13 +0500

    Added Splash Screen

--- ResumeBuilder/src/config/SplashScreen.js
+++ ResumeBuilder/src/config/SplashScreen.js
@@ -1,12 +1,16 @@
 import React,{Component} from 'react';
-import {View,Text} from 'react-native';
+import {View,Text,Image,ActivityIndicator} from 'react-native';
 
 class SplashScreen extends Component{
     render(){
 
       return(
-          <View>
-            <Text> Loading</Text>
+          <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+              <Image
+                style={{width:'80%',height:250}}
+              source={{uri:"https://cdn.dribbble.com/users/36126/screenshots/2228873/rb-logo.png"}}
+              ></Image>
+              <ActivityIndicator size='large'></ActivityIndicator>
           </View>
         )  
     }
